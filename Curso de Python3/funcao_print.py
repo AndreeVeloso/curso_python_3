46373f566fc6d48d3a5879999fb05c973cd68565
andre <andrevvp@outlook.com>
2020-07-03 11:09:11 -0300

    teste 2, testandoo...

---
+++ Curso de Python3/funcao_print.py
@@ -0,0 +1,19 @@
+# COMENTARIO
+
+### COMENTARIOS ###
+
+"""COMENTARIOS TAMBEM"""
+
+### FUNCAO PRINT -> EXIBIR ALGO NA TELA!
+
+print("Exiba na tela")
+print('Andre', 'Veloso')
+print('Andre', 'Veloso', sep='-', end='###')
+print('Joao', 'e', 'Maria', sep='-')
+
+# O Python diferencia letras maiusculas de mininusculas!
+
+# Resoluçao do problema!
+
+print('824', '176', '070', sep='.', end='-')
+print('18')
