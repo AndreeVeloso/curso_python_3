0d5951f0f239f9906568d941ae663d39fa8ca1f1
andre <andrevvp@outlook.com>
2020-07-03 16:07:49 -0300

    Aula de peradores e precedentes aritmeticos

---
+++ Curso de Python3/Variaveis_e_Formatacao.py
@@ -0,0 +1,41 @@
+################### AULA DE VARIAVEIS ###################
+"""
+
+nome = 'Andre' , estilo de atribuicao de variavel
+
+obs: Se inicia com letra, pode conter numeros, para separar as palavrasa ou numeros e letras se usa '_'
+feita em letras minusculas
+
+
+no caso, nome = Andre
+entao
+
+print(nome) -> imprime na tela o valor declarado = Andre
+
+podemos ainda pedir a classificacao da variavel
+
+print(nome, type(nome))
+"""
+
+nome = 'Andre'
+idade = 27
+altura = 1.75
+peso = 75
+e_maior = idade > 18
+imc = peso/ (altura ** 2)
+
+
+"""
+print('Nome: ', nome)
+print('Idade: ', idade)
+print('Altura: ', altura)
+print('E maior de idade: ', e_maior)
+"""
+
+################# RESOLUCAO DO EXERCICIO ####################
+
+
+print(nome, 'tem', idade, 'anos de idade e seu imc e: ', imc)
+
+
+
