08342d76a0fdd31237f159d661198afb39dec2e1
andre <andrevvp@outlook.com>
2020-07-03 11:30:38 -0300

    teste 2, testandoo...

--- Curso de Python3/String_DadosPrimitivos.py
+++ Curso de Python3/String_DadosPrimitivos.py
@@ -11,3 +11,19 @@ print("Andre", 'Strin \n g', """str""")
 # /n -> quebra a linha
 
 ################# DADOS PRIMITIVOS #################
+
+
+# Tipos de Dados Primitivos
+
+# Strings (Str) = Textos dentro de aspas! = assim' ou "assim ou """assim""""
+# Inteiro (Int) = Numeros Inteiros = 123456, 15000, 15, 1 ,0
+# Float ( Numero real flutuante) = 10.5, 10,50, -50.13, 0.0
+# Booleano (Valor logico) = True/False = 10 == 10
+#
+print(type('Andre'))
+
+# No caso acima chamamos a funcao type, que no caso retorna o valor da classe entre os parenteses
+#
+
+
+
