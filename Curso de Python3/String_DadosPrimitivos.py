cf06297eccd91bd604781b2339aa6f31eb97116f
andre <andrevvp@outlook.com>
2020-07-03 12:02:47 -0300

    teste 2, testandoo...

--- Curso de Python3/String_DadosPrimitivos.py
+++ Curso de Python3/String_DadosPrimitivos.py
@@ -23,7 +23,19 @@ print("Andre", 'Strin \n g', """str""")
 print(type('Andre'))
 
 # No caso acima chamamos a funcao type, que no caso retorna o valor da classe entre os parenteses
-#
 
+# print('10' + 10)
 
+# nao se pode somar um texto com um numero inteiro, porem pode-se se juntar um inteiro com outro inteiro,
+# podemos tbm transformar um numero em string do tipo print('10', int('10') que no caso temos uma string com escrita 10 e depois trasnformamos ela em um inteiro, isso se chama type casting!
+
+ ##################### RESOLUCAO DO PROBLEMA DA AULA! ########################
+#String Nome
+print('Andre Vitor Veloso Paim', type('Andre Vitor Veloso Paim'))
+#Int, Idade
+print(27, type(27))
+#Float, Altura
+print(1.75, type(1.75))
+#Bool, Maior de idade
+print(27 >= 18, type(27 >= 18))
 
