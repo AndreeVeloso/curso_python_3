abf3815badcf571bab25c1f60deb286aad7ccbe1
andre <andrevvp@outlook.com>
2020-07-03 11:23:54 -0300

    teste 2, testandoo...

---
+++ Curso de Python3/String_DadosPrimitivos.py
@@ -0,0 +1,13 @@
+################# Strings (str) -> Textos dentro de aspas #################
+
+# Obs -> Podendo ser aspas simples ou aspas duplas e ou ate aspas triplas!
+
+print('Essa é uma string (str).')
+
+print("Essa é uma 'string'(str).")
+
+print("Andre", 'Strin \n g', """str""")
+
+# /n -> quebra a linha
+
+################# DADOS PRIMITIVOS #################
